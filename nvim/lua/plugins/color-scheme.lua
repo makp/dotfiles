423494089a8b68f3cf3e88934ca7b796c82f5242
mak-leibniz <makmiller@gmail.com>
2024-04-25 01:12:41 -0400

    Rename file and change colorscheme to gruvbox

---
+++ nvim/lua/plugins/color-scheme.lua
@@ -0,0 +1,14 @@
+return {
+	"ellisonleao/gruvbox.nvim",
+	priority = 1000,
+	config = function()
+		vim.o.background = "dark"
+		vim.cmd([[colorscheme gruvbox]])
+	end,
+}
+
+-- savq/melange-nvim",
+-- config = function()
+-- vim.opt.termguicolors = true
+-- vim.cmd.colorscheme 'melange'
+-- end
