a3d602bf084142c6a42b971fe01693d2cd9ba243
mak-leibniz <makmiller@gmail.com>
2024-04-28 13:15:24 -0400

    Revise/add git keymaps

--- nvim/lua/plugins/git.lua
+++ nvim/lua/plugins/git.lua
@@ -1,5 +1,5 @@
 return {
-	-- Use gutter to show git info
+	-- Use signs to show git info
 	{
 		"lewis6991/gitsigns.nvim",
 		opts = {
@@ -10,18 +10,17 @@ return {
 				topdelete = { text = "‾" },
 				changedelete = { text = "~" },
 			},
-			--
-			-- Set up mappings
 			on_attach = function(bufnr)
 				local gitsigns = require("gitsigns")
 
+				-- Define func to set up keymaps
 				local function map(mode, l, r, opts)
 					opts = opts or {}
 					opts.buffer = bufnr
 					vim.keymap.set(mode, l, r, opts)
 				end
 
-				-- Navigation
+				-- Hunk navigation
 				map("n", "]h", function()
 					if vim.wo.diff then
 						vim.cmd.normal({ "]h", bang = true })
@@ -29,6 +28,14 @@ return {
 						gitsigns.nav_hunk("next")
 					end
 				end, { desc = "Jump to next git [h]unk" })
+
+				map("n", "]H", function()
+					if vim.wo.diff then
+						vim.cmd.normal({ "]H", bang = true })
+					else
+						gitsigns.nav_hunk("last")
+					end
+				end, { desc = "Jump to last git [h]unk" })
 
 				map("n", "[h", function()
 					if vim.wo.diff then
@@ -38,35 +45,44 @@ return {
 					end
 				end, { desc = "Jump to previous git [h]unk" })
 
+				map("n", "[H", function()
+					if vim.wo.diff then
+						vim.cmd.normal({ "[H", bang = true })
+					else
+						gitsigns.nav_hunk("first")
+					end
+				end, { desc = "Jump to first git [h]unk" })
+
 				-- Actions
 				-- Visual mode
-				map("v", "<leader>gs", function()
+				map("v", "<localleader>gs", function()
 					gitsigns.stage_hunk({ vim.fn.line("."), vim.fn.line("v") })
 				end)
-				map("v", "<leader>gr", function()
+				map("v", "<localleader>gr", function()
 					gitsigns.reset_hunk({ vim.fn.line("."), vim.fn.line("v") })
 				end)
 				-- Normal mode
-				map("n", "<leader>gs", gitsigns.stage_hunk)
-				map("n", "<leader>gS", gitsigns.stage_buffer)
-				map("n", "<leader>gr", gitsigns.reset_hunk)
-				map("n", "<leader>gR", gitsigns.reset_buffer)
-				map("n", "<leader>gu", gitsigns.undo_stage_hunk)
-				map("n", "<leader>gp", gitsigns.preview_hunk)
-				map("n", "<leader>gb", function()
+				map("n", "<localleader>gs", gitsigns.stage_hunk)
+				map("n", "<localleader>gu", gitsigns.undo_stage_hunk) -- undo last stage_hunk
+				map("n", "<localleader>gS", gitsigns.stage_buffer)
+				map("n", "<localleader>gr", gitsigns.reset_hunk) -- at the cursor position
+				map("n", "<localleader>gR", gitsigns.reset_buffer)
+				map("n", "<localleader>gp", gitsigns.preview_hunk)
+				map("n", "<localleader>gb", function()
 					gitsigns.blame_line({ full = true })
 				end)
-				map("n", "<leader>gd", gitsigns.diffthis, { desc = "git [d]iff against index" })
-				map("n", "<leader>gD", function()
+				map("n", "<localleader>gd", gitsigns.diffthis, { desc = "git [d]iff against index" })
+				map("n", "<localleader>gD", function()
 					gitsigns.diffthis("@")
 				end, { desc = "git [D]iff against last commit" })
 				--
 				-- Toggles
-				map("n", "<leader>gtb", gitsigns.toggle_current_line_blame)
-				map("n", "<leader>gtd", gitsigns.toggle_deleted)
+				map("n", "<localleader>gtb", gitsigns.toggle_current_line_blame)
+				map("n", "<localleader>gtd", gitsigns.toggle_deleted)
+				map("n", "<localleader>gtw", gitsigns.toggle_word_diff)
 
-				-- Text object
-				-- map({ "o", "x" }, "ih", ":<C-U>Gitsigns select_hunk<CR>")
+				-- Hunk object
+				map({ "o", "x" }, "ih", ":<C-U>Gitsigns select_hunk<CR>")
 			end,
 		},
 	},
@@ -88,7 +104,7 @@ return {
 		-- setting the keybinding for LazyGit with 'keys' is recommended in
 		-- order to load the plugin when the command is run for the first time
 		keys = {
-			{ "<leader>gl", "<cmd>LazyGit<cr>", desc = "LazyGit" },
+			{ "<localleader>gg", "<cmd>LazyGit<cr>", desc = "LazyGit" },
 		},
 	},
 }
