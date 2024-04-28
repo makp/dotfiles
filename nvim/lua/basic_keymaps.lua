f7ddb804dd3e564d8bc1ccd0ce2dd6f9c5097d8d
mak-leibniz <makmiller@gmail.com>
2024-04-28 00:38:05 -0400

    Comment keybindings for windows navigation

--- nvim/lua/basic_keymaps.lua
+++ nvim/lua/basic_keymaps.lua
@@ -8,13 +8,13 @@ vim.g.maplocalleader = " "
 vim.keymap.set("n", "<leader>dv", vim.diagnostic.open_float, { desc = "Show diagnostic [E]rror messages" })
 vim.keymap.set("n", "<leader>dl", vim.diagnostic.setloclist, { desc = "Open diagnostic [Q]uickfix list" })
 
---  Use CTRL+<hjkl> to switch between windows
+--[[ --  Use CTRL+<hjkl> to switch between windows
 --  See `:help wincmd` for a list of all window commands
 vim.keymap.set("n", "<C-h>", "<C-w><C-h>", { desc = "Move focus to the left window" })
 vim.keymap.set("n", "<C-l>", "<C-w><C-l>", { desc = "Move focus to the right window" })
 vim.keymap.set("n", "<C-j>", "<C-w><C-j>", { desc = "Move focus to the lower window" })
 vim.keymap.set("n", "<C-k>", "<C-w><C-k>", { desc = "Move focus to the upper window" })
-
+]]
 -- Set global square brackets keymaps (similar to vim-unimpaired)
 --
 -- Buffers
