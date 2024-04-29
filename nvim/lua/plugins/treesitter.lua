24decfbdf3bfc4d5b1276cacb23653b68e6633d8
mak-turing <makmiller@gmail.com>
2024-04-28 22:48:34 -0400

    Add nvim-treesitter-textobjects and clean treesitter config

--- nvim/lua/plugins/treesitter.lua
+++ nvim/lua/plugins/treesitter.lua
@@ -1,44 +1,61 @@
--- Highlight, edit, and navigate code
---
+-- Syntax
 return {
-	"nvim-treesitter/nvim-treesitter",
-	build = ":TSUpdate",
-	opts = {
-		ensure_installed = {
-			"bash",
-			"c",
-			"html",
-			"lua",
-			"luadoc",
-			"markdown",
-			"vim",
-			"vimdoc",
-			"python",
-		},
-		--
-		-- Autoinstall languages that are not installed
-		auto_install = true,
-		highlight = {
-			enable = true,
-			-- Some languages depend on vim's regex highlighting system (such as Ruby) for indent rules.
-			--  If you are experiencing weird indenting issues, add the language to
-			--  the list of additional_vim_regex_highlighting and disabled languages for indent.
-			additional_vim_regex_highlighting = { "ruby" },
-		},
-		indent = { enable = true, disable = { "ruby" } },
+	{
+		"nvim-treesitter/nvim-treesitter-textobjects",
 	},
-	config = function(_, opts)
+	{
+		"nvim-treesitter/nvim-treesitter",
+		build = ":TSUpdate",
+		opts = {
+			ensure_installed = {
+				"bash",
+				"html",
+				"lua",
+				"luadoc",
+				"markdown",
+				"vim",
+				"vimdoc",
+				"python",
+			},
+			-- Autoinstall languages that are not installed
+			auto_install = true,
+
+			-- Syntax highlighting
+			highlight = {
+				enable = true,
+			},
+
+			-- Indentation for the = operator
+			indent = {
+				enable = true,
+			},
+
+			-- Treesitter textobjects
+			textobjects = {
+				select = {
+					enable = true,
+
+					-- Automatically jump forward to textobjects
+					lookahead = true,
+
+					keymaps = {
+						-- Capture groups are defined in textobjects.scm
+						["af"] = "@function.outer",
+						["if"] = "@function.inner",
+						-- ["ac"] = "@class.outer",
+						-- you can optionally set descriptions to the mappings (used in the desc parameter of nvim_buf_set_keymap
+						-- ["ic"] = { query = "@class.inner", desc = "Select inner part of a class region" },
+					},
+				},
+			},
+		},
 		-- [[ Configure Treesitter ]] See `:help nvim-treesitter`
-
-		-- Prefer git instead of curl in order to improve connectivity in some environments
-		require("nvim-treesitter.install").prefer_git = true
-		---@diagnostic disable-next-line: missing-fields
-		require("nvim-treesitter.configs").setup(opts)
-
-		-- Additional nvim-treesitter modules:
-		--
-		--    - Incremental selection: Included, see `:help nvim-treesitter-incremental-selection-mod`
-		--    - Show your current context: https://github.com/nvim-treesitter/nvim-treesitter-context
-		--    - Treesitter + textobjects: https://github.com/nvim-treesitter/nvim-treesitter-textobjects
-	end,
+		-- Run :TSModuleInfo to check if a module is enabled
+		config = function(_, opts)
+			-- Prefer git instead of curl in order to improve connectivity in some environments
+			require("nvim-treesitter.install").prefer_git = true
+			-- Setup nvim-treesitter
+			require("nvim-treesitter.configs").setup(opts)
+		end,
+	},
 }
