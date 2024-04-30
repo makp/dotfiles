971b36309ec8cebce5cb36f1e8066a77726d7d52
mak-turing <makmiller@gmail.com>
2024-04-30 11:24:16 -0400

    Complete with Enter

--- nvim/lua/plugins/completion.lua
+++ nvim/lua/plugins/completion.lua
@@ -64,7 +64,7 @@ return {
 					-- Accept the completion.
 					--  This will auto-import if your LSP supports it.
 					--  This will expand snippets if the LSP sent a snippet.
-					["<C-j>"] = cmp.mapping.confirm({ select = true }),
+					["<CR>"] = cmp.mapping.confirm({ select = true }),
 
 					-- Manually trigger a completion from nvim-cmp.
 					--  Generally you don't need this, because nvim-cmp will display
