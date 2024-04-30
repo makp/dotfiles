c8eff7c5db9f86be2c73ca042bb5aefcae8d7aa0
mak-leibniz <makmiller@gmail.com>
2024-04-29 21:12:45 -0400

    Use localleader for lsp keymaps

--- nvim/lua/plugins/lsp.lua
+++ nvim/lua/plugins/lsp.lua
@@ -26,44 +26,48 @@ return {
 					vim.keymap.set("n", keys, func, { buffer = event.buf, desc = "LSP: " .. desc })
 				end
 
-				-- Jump to the definition of the word under your cursor.
-				--  To jump back, press <C-t>
+				-- Jump to the definition of the word under your cursor
+				--  Press <C-t> to jump back
 				map("gd", require("telescope.builtin").lsp_definitions, "[G]oto [D]efinition")
+
+				-- Jump to the declaration of the word under your cursor
+				--  For example, in C this would take you to the header.
+				map("<localleader>cD", vim.lsp.buf.declaration, "[G]oto [D]eclaration")
 
 				-- Find references for the word under your cursor.
 				map("gr", require("telescope.builtin").lsp_references, "[G]oto [R]eferences")
 
 				-- Jump to the implementation of the word under your cursor.
-				--  Useful when your language has ways of declaring types without an actual implementation.
-				map("gI", require("telescope.builtin").lsp_implementations, "[G]oto [I]mplementation")
+				--  Useful when your language has ways of declaring types without an actual implementation
+				map("<localleader>cI", require("telescope.builtin").lsp_implementations, "[G]oto [I]mplementation")
 
 				-- Jump to the type of the word under your cursor.
 				--  Useful when you're not sure what type a variable is and you want to see
 				--  the definition of its *type*, not where it was *defined*.
-				-- map('<leader>ct', require('telescope.builtin').lsp_type_definitions, 'Type [D]efinition')
+				map("<localleader>ct", require("telescope.builtin").lsp_type_definitions, "Type [D]efinition")
 
 				-- Fuzzy find all the symbols in your current document.
 				--  Symbols are things like variables, functions, types, etc.
-				map("<leader>ss", require("telescope.builtin").lsp_document_symbols, "[D]ocument [S]ymbols")
+				map("<localleader>ss", require("telescope.builtin").lsp_document_symbols, "[D]ocument [S]ymbols")
 
 				-- Fuzzy find all the symbols in your current workspace.
 				--  Similar to document symbols, except searches over your entire project.
-				map("<leader>fs", require("telescope.builtin").lsp_dynamic_workspace_symbols, "[W]orkspace [S]ymbols")
+				map(
+					"<localleader>cs",
+					require("telescope.builtin").lsp_dynamic_workspace_symbols,
+					"[W]orkspace [S]ymbols"
+				)
 
 				-- Rename the variable under your cursor.
 				--  Most Language Servers support renaming across files, etc.
-				map("<leader>cr", vim.lsp.buf.rename, "[c]ode [r]ename")
+				map("<localleader>cr", vim.lsp.buf.rename, "[c]ode [r]ename")
 
 				-- Execute a code action
-				map("<leader>ca", vim.lsp.buf.code_action, "[c]ode [a]ction")
+				map("<localleader>ca", vim.lsp.buf.code_action, "[c]ode [a]ction")
 
 				-- Opens a popup with documentation about the word under your cursor
 				--  See `:help K` for why this keymap.
 				map("K", vim.lsp.buf.hover, "Hover Documentation")
-
-				-- WARN: This is not Goto Definition, this is Goto Declaration.
-				--  For example, in C this would take you to the header.
-				-- map('gD', vim.lsp.buf.declaration, '[G]oto [D]eclaration')
 
 				-- The following two autocommands are used to highlight references of the
 				-- word under your cursor when your cursor rests there for a little while.
@@ -86,10 +90,9 @@ return {
 					})
 				end
 
-				-- Enable inlay hints in your code, if supported by LSP
-				-- This may be unwanted, since they displace some of your code
+				-- Enable inlay hints in your code
 				if client and client.server_capabilities.inlayHintProvider and vim.lsp.inlay_hint then
-					map("<leader>th", function()
+					map("<localleader>ct", function()
 						vim.lsp.inlay_hint.enable(not vim.lsp.inlay_hint.is_enabled())
 					end, "[T]oggle Inlay [H]ints")
 				end
