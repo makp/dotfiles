bc6306b7a09f5be482d3d7dba1e1993c4e584e59
mak-leibniz <makmiller@gmail.com>
2024-04-29 22:41:52 -0400

    Use a gruvbox scheme with treesitter support

--- nvim/lua/plugins/color-scheme.lua
+++ nvim/lua/plugins/color-scheme.lua
@@ -1,14 +1,21 @@
 return {
-	"ellisonleao/gruvbox.nvim",
-	priority = 1000,
+	"sainnhe/gruvbox-material",
 	config = function()
-		vim.o.background = "dark"
-		vim.cmd([[colorscheme gruvbox]])
+		-- Use termguicolors if available
+		if vim.fn.has("termguicolors") == 1 then
+			vim.opt.termguicolors = true
+		end
+
+		-- Set background
+		vim.opt.background = "dark"
+
+		-- Set contrast
+		vim.g.gruvbox_material_background = "medium"
+
+		-- For better performance
+		vim.g.gruvbox_material_better_performance = 1
+
+		-- Load the colorscheme
+		vim.cmd([[colorscheme gruvbox-material]])
 	end,
 }
-
--- savq/melange-nvim",
--- config = function()
--- vim.opt.termguicolors = true
--- vim.cmd.colorscheme 'melange'
--- end
