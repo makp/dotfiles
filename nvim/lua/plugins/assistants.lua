7de3853cbae8cbd7d1b10821d3e94f40c8eb4e22
mak-turing <makmiller@gmail.com>
2024-05-02 16:36:10 -0400

    Add key for explaining code via gpt

--- nvim/lua/plugins/assistants.lua
+++ nvim/lua/plugins/assistants.lua
@@ -2,7 +2,14 @@ return {
 	"jackMort/ChatGPT.nvim",
 	event = "VeryLazy",
 	config = function()
-		require("chatgpt").setup()
+		require("chatgpt").setup({
+			vim.api.nvim_set_keymap(
+				"v",
+				"<leader>ce",
+				":ChatGPTRun explain_code<CR>",
+				{ noremap = true, silent = true }
+			),
+		})
 	end,
 	dependencies = {
 		"MunifTanjim/nui.nvim",
