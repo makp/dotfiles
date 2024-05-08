d22cd4d6733df19a4968868dcac1e1245edbf554
mak-leibniz <makmiller@gmail.com>
2024-05-07 23:54:43 -0400

    Add list of options

--- nvim/after/ftplugin/tex.lua
+++ nvim/after/ftplugin/tex.lua
@@ -8,26 +8,55 @@ local function texlab_command(cmd, params)
 	})
 end
 
--- Change the current latex environment
-function LaTeX_change_env()
-	-- Prompt user for the new environment name
-	local newName = vim.fn.input("New environment name: ")
-	if newName == nil then
+-- Define table	with list of LaTeX environments
+local dic = {}
+dic.environments = {
+	"equation",
+	"equation*",
+	"table",
+	"tabular",
+	"tabular*",
+	"itemize",
+	"enumerate",
+	"description",
+	"figure",
+	"figure*",
+}
+
+-- Wrapper func on `texlab.changeEnvironment` command
+function dic.TexlabChangeEnv(new_env)
+	if new_env == nil then
 		print("No environment name provided")
 	else
 		-- Get current positions pars
 		local params = vim.lsp.util.make_position_params()
 		-- Add the new name to the params
-		params["newName"] = newName
+		params["newName"] = new_env
 		-- Execute the command
 		texlab_command("texlab.changeEnvironment", params)
 	end
+end
+
+-- Use vim.ui.select to select an environment
+function LaTeXChangeEnv()
+	vim.ui.select(dic.environments, {
+		prompt = "Select an environment: ",
+		format_item = function(item)
+			return item
+		end,
+	}, function(choice)
+		if choice then
+			dic.TexlabChangeEnv(choice)
+		else
+			print("No environment selected!")
+		end
+	end)
 end
 
 vim.api.nvim_buf_set_keymap(
 	0,
 	"n",
 	"<localleader>ce",
-	"<cmd>lua LaTeX_change_env()<CR>",
+	"<cmd>lua LaTeXChangeEnv()<CR>",
 	{ noremap = true, silent = true }
 )
