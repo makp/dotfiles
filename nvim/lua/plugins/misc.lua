c435317cb3dc4810ccb16a1e8c12203c093e01f6
mak-leibniz <makmiller@gmail.com>
2024-05-11 23:05:56 -0400

    Change orgmode prefix to avoid conflict with other keybindings

--- nvim/lua/plugins/misc.lua
+++ nvim/lua/plugins/misc.lua
@@ -119,6 +119,7 @@ return {
 			require("orgmode").setup({
 				org_agenda_files = "~/elisp/agendas/**/*",
 				-- org_default_notes_file = '~/orgfiles/refile.org',
+				mappings = { prefix = "<leader>O" },
 			})
 		end,
 	},
