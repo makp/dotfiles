ad23eebc13a95aefa9501ca17d69e9ff1a2a883b
mak-leibniz <makmiller@gmail.com>
2024-05-13 23:21:09 -0400

    Change keybinding latex cmds

--- nvim/after/ftplugin/tex.lua
+++ nvim/after/ftplugin/tex.lua
@@ -3,11 +3,10 @@
 vim.api.nvim_buf_set_keymap(
 	0,
 	"n",
-	"<localleader>le",
+	"<localleader>te",
 	"<cmd>lua require('texlab_setup').LaTeXChangeEnv()<CR>",
 	{ noremap = true, silent = true }
 )
 
-vim.api.nvim_buf_set_keymap(0, "n", "<localleader>lc", "<cmd>TexlabBuild<CR>", { noremap = true, silent = true })
-
-vim.api.nvim_buf_set_keymap(0, "n", "<localleader>ls", "<cmd>TexlabForward<CR>", { noremap = true, silent = true })
+vim.api.nvim_buf_set_keymap(0, "n", "<localleader>tc", "<cmd>TexlabBuild<CR>", { noremap = true, silent = true })
+vim.api.nvim_buf_set_keymap(0, "n", "<localleader>ts", "<cmd>TexlabForward<CR>", { noremap = true, silent = true })
