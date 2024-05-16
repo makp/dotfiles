c09c2ed99263d8ab89a23292501337ee2284ff9b
mak-leibniz <makmiller@gmail.com>
2024-05-16 00:30:29 -0400

    Add telescope-based func to search header in md files

--- nvim/after/ftplugin/markdown.lua
+++ nvim/after/ftplugin/markdown.lua
@@ -3,3 +3,15 @@ vim.g.markdown_folding = 1
 
 -- Enable spell checking
 vim.opt_local.spell = true
+
+-- Custom function for markdown headers
+function MarkdownHeader()
+	require("telescope.builtin").grep_string({
+		prompt_title = "Markdown Headers",
+		use_regex = true,
+		search = "^#",
+		search_dirs = { vim.fn.expand("%") },
+	})
+end
+
+vim.api.nvim_set_keymap("n", "<localleader>mh", "<cmd>lua MarkdownHeader()<CR>", { noremap = true, silent = true })
