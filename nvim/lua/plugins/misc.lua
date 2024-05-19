efd96b220cedb8e79a0aedff4ee42dd5085e9253
mak-leibniz <makmiller@gmail.com>
2024-05-18 20:52:08 -0400

    Configure nvim-lint

--- nvim/lua/plugins/misc.lua
+++ nvim/lua/plugins/misc.lua
@@ -75,6 +75,33 @@ return {
 		},
 	},
 
+	-- Linting
+	-- `nvim-lint` reports its results through the `vim.diagnostic` module.
+	{
+		"mfussenegger/nvim-lint",
+		event = { "BufReadPre", "BufNewFile" },
+		config = function()
+			local lint = require("lint")
+			lint.linters_by_ft = {
+				-- markdown = { "vale" },
+				-- text = { "vale" },
+			}
+
+			-- Create autocommand which carries out the actual linting
+			-- on the specified events.
+			local lint_augroup = vim.api.nvim_create_augroup("lint", { clear = true })
+			vim.api.nvim_create_autocmd({ "BufEnter", "BufWritePost", "InsertLeave" }, {
+				group = lint_augroup,
+				callback = function()
+					-- Run linters as defined by `lint.linters_by_ft`
+					-- require("lint").try_lint()
+					-- Run `codespell` on all files
+					require("lint").try_lint({ "codespell" })
+				end,
+			})
+		end,
+	},
+
 	-- Highlight TODOs in comments
 	{
 		"folke/todo-comments.nvim",
