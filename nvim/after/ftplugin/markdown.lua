7aabaff039b07b0dfc18c670c35c5370d163d6f5
mak-turing <makmiller@gmail.com>
2024-05-22 20:58:01 -0400

    Set up `Sniprun` for running codeblocks in markdown

--- nvim/after/ftplugin/markdown.lua
+++ nvim/after/ftplugin/markdown.lua
@@ -1,7 +1,7 @@
 -- Enable spell checking
 vim.opt_local.spell = true
 
--- Custom function for markdown headers
+-- Custom function for searching markdown headers
 function MarkdownHeader()
 	require("telescope.builtin").grep_string({
 		prompt_title = "Markdown Headers",
@@ -11,4 +11,15 @@ function MarkdownHeader()
 	})
 end
 
-vim.keymap.set("n", "<localleader>mh", "<cmd>lua MarkdownHeader()<CR>", { noremap = true, silent = true })
+vim.api.nvim_buf_set_keymap(
+	0,
+	"n",
+	"<localleader>mh",
+	"<cmd>lua MarkdownHeader()<CR>",
+	{ noremap = true, silent = true }
+)
+
+-- Custom keybindings for running code (`SnipRun`)
+vim.api.nvim_buf_set_keymap(0, "v", "<localleader>sc", "<Plug>SnipRun", { silent = true })
+vim.api.nvim_buf_set_keymap(0, "n", "<localleader>sl", "<Plug>SnipRun", { silent = true })
+vim.api.nvim_buf_set_keymap(0, "n", "<localleader>sc", "<Plug>SnipRunOperator", { silent = true })
