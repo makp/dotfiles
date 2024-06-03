8757f14d78d1da0f91ad84aad97515a8d4981219
mak-turing <makmiller@gmail.com>
2024-06-03 11:32:55 -0400

    Set up nvim-dap

---
+++ nvim/lua/plugins/debugger.lua
@@ -0,0 +1,74 @@
+return {
+	"mfussenegger/nvim-dap",
+	dependencies = {
+		-- Debugger UI
+		"rcarriga/nvim-dap-ui",
+
+		-- Required dependency for nvim-dap-ui
+		"nvim-neotest/nvim-nio",
+
+		-- Use Mason to install debug adapters
+		"williamboman/mason.nvim",
+		"jay-babu/mason-nvim-dap.nvim",
+
+		-- DAP lang extensions
+		"mfussenegger/nvim-dap-python",
+	},
+	config = function()
+		local dap = require("dap")
+		local dapui = require("dapui")
+
+		require("mason-nvim-dap").setup({
+			-- Makes a best effort to setup the various debuggers with
+			-- reasonable debug configurations
+			automatic_installation = true,
+
+			-- You can provide additional configuration to the handlers,
+			-- see mason-nvim-dap README for more information
+			handlers = {},
+
+			-- Add the specific lang debuggers
+			ensure_installed = {},
+		})
+
+		-- Basic debugging keymaps
+		vim.keymap.set("n", "<F5>", dap.continue, { desc = "Debug: Start/Continue" })
+		vim.keymap.set("n", "<F1>", dap.step_into, { desc = "Debug: Step Into" })
+		vim.keymap.set("n", "<F2>", dap.step_over, { desc = "Debug: Step Over" })
+		vim.keymap.set("n", "<F3>", dap.step_out, { desc = "Debug: Step Out" })
+		vim.keymap.set("n", "<localleader>b", dap.toggle_breakpoint, { desc = "Debug: Toggle Breakpoint" })
+		vim.keymap.set("n", "<localleader>B", function()
+			dap.set_breakpoint(vim.fn.input("Breakpoint condition: "))
+		end, { desc = "Debug: Set Breakpoint" })
+
+		-- Dap UI setup
+		-- For more information, see :help nvim-dap-ui
+		dapui.setup({
+			icons = { expanded = "▾", collapsed = "▸", current_frame = "*" },
+			controls = {
+				icons = {
+					pause = "⏸",
+					play = "▶",
+					step_into = "⏎",
+					step_over = "⏭",
+					step_out = "⏮",
+					step_back = "b",
+					run_last = "▶▶",
+					terminate = "⏹",
+					disconnect = "⏏",
+				},
+			},
+		})
+
+		-- Toggle to see last session result. Without this, you can't see session
+		-- output in case of unhandled exception.
+		vim.keymap.set("n", "<F7>", dapui.toggle, { desc = "Debug: See last session result." })
+
+		dap.listeners.after.event_initialized["dapui_config"] = dapui.open
+		dap.listeners.before.event_terminated["dapui_config"] = dapui.close
+		dap.listeners.before.event_exited["dapui_config"] = dapui.close
+
+		-- Install lang specific config
+		--
+	end,
+}
