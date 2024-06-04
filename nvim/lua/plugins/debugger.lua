8a4cc358031418de686e6a87ad592049e8861194
mak-leibniz <makmiller@gmail.com>
2024-06-04 00:47:43 -0400

    Customize DAP and add python debugger

--- nvim/lua/plugins/debugger.lua
+++ nvim/lua/plugins/debugger.lua
@@ -3,15 +3,13 @@ return {
 	dependencies = {
 		-- Debugger UI
 		"rcarriga/nvim-dap-ui",
-
-		-- Required dependency for nvim-dap-ui
 		"nvim-neotest/nvim-nio",
 
 		-- Use Mason to install debug adapters
 		"williamboman/mason.nvim",
 		"jay-babu/mason-nvim-dap.nvim",
 
-		-- DAP lang extensions
+		-- DAP extension for python
 		"mfussenegger/nvim-dap-python",
 	},
 	config = function()
@@ -19,56 +17,39 @@ return {
 		local dapui = require("dapui")
 
 		require("mason-nvim-dap").setup({
-			-- Makes a best effort to setup the various debuggers with
-			-- reasonable debug configurations
 			automatic_installation = true,
 
-			-- You can provide additional configuration to the handlers,
-			-- see mason-nvim-dap README for more information
 			handlers = {},
 
 			-- Add the specific lang debuggers
-			ensure_installed = {},
+			ensure_installed = { "python" },
 		})
 
 		-- Basic debugging keymaps
-		vim.keymap.set("n", "<F5>", dap.continue, { desc = "Debug: Start/Continue" })
-		vim.keymap.set("n", "<F1>", dap.step_into, { desc = "Debug: Step Into" })
-		vim.keymap.set("n", "<F2>", dap.step_over, { desc = "Debug: Step Over" })
-		vim.keymap.set("n", "<F3>", dap.step_out, { desc = "Debug: Step Out" })
-		vim.keymap.set("n", "<localleader>b", dap.toggle_breakpoint, { desc = "Debug: Toggle Breakpoint" })
-		vim.keymap.set("n", "<localleader>B", function()
+		vim.keymap.set("n", "<localleader>ds", dap.continue, { desc = "Debug: Start/Continue" })
+		vim.keymap.set("n", "<localleader>di", dap.step_into, { desc = "Debug: Step Into" })
+		vim.keymap.set("n", "<localleader>dn", dap.step_over, { desc = "Debug: Step Over" })
+		vim.keymap.set("n", "<localleader>do", dap.step_out, { desc = "Debug: Step Out" })
+		vim.keymap.set("n", "<localleader>db", dap.toggle_breakpoint, { desc = "Debug: Toggle Breakpoint" })
+		vim.keymap.set("n", "<localleader>dB", function()
 			dap.set_breakpoint(vim.fn.input("Breakpoint condition: "))
 		end, { desc = "Debug: Set Breakpoint" })
+		vim.keymap.set("n", "<localleader>dr", dap.repl.open, { desc = "Debug: Open REPL" })
 
 		-- Dap UI setup
-		-- For more information, see :help nvim-dap-ui
-		dapui.setup({
-			icons = { expanded = "▾", collapsed = "▸", current_frame = "*" },
-			controls = {
-				icons = {
-					pause = "⏸",
-					play = "▶",
-					step_into = "⏎",
-					step_over = "⏭",
-					step_out = "⏮",
-					step_back = "b",
-					run_last = "▶▶",
-					terminate = "⏹",
-					disconnect = "⏏",
-				},
-			},
-		})
+		dapui.setup(
+			-- 	{
+			-- 	icons = { expanded = "▾", collapsed = "▸", current_frame = "*" },
+			-- }
+		)
 
 		-- Toggle to see last session result. Without this, you can't see session
 		-- output in case of unhandled exception.
-		vim.keymap.set("n", "<F7>", dapui.toggle, { desc = "Debug: See last session result." })
+		vim.keymap.set("n", "<localleader>dt", dapui.toggle, { desc = "Debug: See last session result." })
 
+		-- Use DAP events to open and close the UI
 		dap.listeners.after.event_initialized["dapui_config"] = dapui.open
 		dap.listeners.before.event_terminated["dapui_config"] = dapui.close
 		dap.listeners.before.event_exited["dapui_config"] = dapui.close
-
-		-- Install lang specific config
-		--
 	end,
 }
