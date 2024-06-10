ebaffd501d8d07b9211b7b070fc553278b808df2
mak-turing <makmiller@gmail.com>
2024-06-10 10:21:46 -0400

    Fix conflicting keybinding for tex files; Update submodule commit `x-configs`

--- nvim/after/ftplugin/tex.lua
+++ nvim/after/ftplugin/tex.lua
@@ -27,4 +27,4 @@ vim.api.nvim_buf_set_keymap(
 vim.api.nvim_buf_set_keymap(0, "n", "<localleader>tp", "<cmd>TexlabForward<CR>", { noremap = true, silent = true })
 
 -- Insert BibTeX references
-vim.api.nvim_buf_set_keymap(0, "n", "<localleader>tb", "<cmd>Telescope bibtex<CR>", { noremap = true, silent = true })
+vim.api.nvim_buf_set_keymap(0, "n", "<localleader>tr", "<cmd>Telescope bibtex<CR>", { noremap = true, silent = true })
