49e4ecf9add6b9563127bd5cbbb4356aff4fb650
mak-leibniz <makmiller@gmail.com>
2024-06-23 00:38:44 -0400

    Stream the output instead

--- scripts/online_search.py
+++ scripts/online_search.py
@@ -7,9 +7,9 @@ from openai import OpenAI
 
 API_KEY = os.getenv("PPLX_API_KEY")
 MODEL = "llama-3-sonar-large-32k-online"
+BASE_URL = "https://api.perplexity.ai"
 
-
-CLIENT = OpenAI(api_key=API_KEY, base_url="https://api.perplexity.ai")
+CLIENT = OpenAI(api_key=API_KEY, base_url=BASE_URL)
 
 PROSE_STYLE = """
 You are an artificial intelligence assistant and you need to engage in a helpful and detailed conversation with a user.
@@ -24,8 +24,10 @@ def answer_question(question):
             {"role": "system", "content": PROSE_STYLE},
             {"role": "user", "content": question},
         ],
+        stream=True,
     )
-    print(response_stream.choices[0].message.content)
+    for chunk in response_stream:
+        print(chunk.choices[0].delta.content or "", end="")
 
 
 if __name__ == "__main__":
