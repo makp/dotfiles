f7132aa334a4d10fdaeb7633128d5f86e697fdba
mak-leibniz <makmiller@gmail.com>
2024-06-23 23:02:11 -0400

    Use M-jkhl for switching windows

--- nvim/lua/plugins/windows.lua
+++ nvim/lua/plugins/windows.lua
@@ -12,20 +12,16 @@ return {
 	-- Integrate nvim window navigation with tmux
 	{
 		"christoomey/vim-tmux-navigator",
-		cmd = {
-			"TmuxNavigateLeft",
-			"TmuxNavigateDown",
-			"TmuxNavigateUp",
-			"TmuxNavigateRight",
-			"TmuxNavigatePrevious",
-		},
-		keys = {
-			{ "<c-h>", "<cmd><C-U>TmuxNavigateLeft<cr>" },
-			{ "<c-j>", "<cmd><C-U>TmuxNavigateDown<cr>" },
-			{ "<c-k>", "<cmd><C-U>TmuxNavigateUp<cr>" },
-			{ "<c-l>", "<cmd><C-U>TmuxNavigateRight<cr>" },
-			{ "<c-\\>", "<cmd><C-U>TmuxNavigatePrevious<cr>" },
-		},
+		init = function()
+			vim.g.tmux_navigator_no_mappings = 1
+			vim.g.tmux_navigator_disable_when_zoomed = 1
+		end,
+		config = function()
+			vim.keymap.set("n", "<A-h>", "<cmd>TmuxNavigateLeft<CR>")
+			vim.keymap.set("n", "<A-j>", "<cmd>TmuxNavigateDown<CR>")
+			vim.keymap.set("n", "<A-k>", "<cmd>TmuxNavigateUp<CR>")
+			vim.keymap.set("n", "<A-l>", "<cmd>TmuxNavigateRight<CR>")
+		end,
 	},
 
 	-- Open nvim windows in browser
