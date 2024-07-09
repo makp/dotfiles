14dcced694bcd65762e68da6fd28d29440e1e397
mak-leibniz <makmiller@gmail.com>
2024-07-09 01:30:24 -0400

    Rewrite script so that a viewer/editor can be passed as an argument

--- scripts/fzf_files_and_dirs.sh
+++ scripts/fzf_files_and_dirs.sh
@@ -1,5 +1,8 @@
 #!/usr/bin/env bash
 
+# Check if an argument is passed. Default to nvim if not
+OPEN_CMD="${1:-nvim}"
+
 # Define the commands to find directories, files, and both
 FIND_DIRS_CMD="fd --type d --strip-cwd-prefix --no-ignore --hidden --exclude .git"
 FIND_FILES_CMD="fd --type f --strip-cwd-prefix --no-ignore --hidden --exclude .git"
@@ -7,15 +10,15 @@ FIND_FILES_AND_DIRS_CMD="fd --strip-cwd-prefix --no-ignore --hidden --exclude .g
 
 # Use fzf to select a directory or file
 $FIND_FILES_AND_DIRS_CMD | fzf \
-  --multi --bind 'enter:become(nvim {+})' \
-  --prompt 'All> ' \
-  --header 'ALT-D: Dirs / ALT-F: Files / ALT-A: All / ALT-P: Preview' \
+  --multi --bind "enter:become($OPEN_CMD {+})" \
+  --prompt "All> " \
+  --header "ALT-D: Dirs / ALT-F: Files / ALT-A: All / ALT-P: Preview" \
   --bind "alt-d:change-prompt(Dirs> )+reload(eval '$FIND_DIRS_CMD')" \
   --bind "alt-f:change-prompt(Files> )+reload(eval '$FIND_FILES_CMD')" \
   --bind "alt-a:change-prompt(All> )+reload(eval '$FIND_FILES_AND_DIRS_CMD')" \
-  --bind 'alt-p:toggle-preview' \
-  --preview '[[ -d {} ]] && eza -T --level=2 --git --colour=always --icons {} || bat --color=always --style=numbers,header,grid --line-range=:500 {}' \
-  --preview-window='hidden' \
-  --bind 'ctrl-u:preview-up' \
-  --bind 'ctrl-d:preview-down' \
+  --bind "alt-p:toggle-preview" \
+  --preview "[[ -d {} ]] && eza -T --level=2 --git --colour=always --icons {} || bat --color=always --style=numbers,header,grid --line-range=:500 {}" \
+  --preview-window="hidden" \
+  --bind "ctrl-u:preview-up" \
+  --bind "ctrl-d:preview-down" \
   --height 50% --layout=reverse
