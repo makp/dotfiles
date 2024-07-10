ca0e6295ec6eea149d90ef5cf2d1692e92955b37
mak-leibniz <makmiller@gmail.com>
2024-07-10 01:32:22 -0400

    Change a couple of keybindings for fzf-lua preview

--- nvim/lua/plugins/fuzzy-search.lua
+++ nvim/lua/plugins/fuzzy-search.lua
@@ -6,7 +6,25 @@ return {
 		config = function()
 			local fzflua = require("fzf-lua")
 
-			fzflua.setup({})
+			fzflua.setup({
+				keymap = {
+					builtin = {
+						["<F1>"] = "toggle-help",
+						["<F2>"] = "toggle-fullscreen",
+						-- Only valid with the 'builtin' previewer
+						["<F3>"] = "toggle-preview-wrap",
+						["<F4>"] = "toggle-preview",
+						-- Rotate preview clockwise/counter-clockwise
+						["<F5>"] = "toggle-preview-ccw",
+						["<F6>"] = "toggle-preview-cw",
+						["<A-w>"] = "toggle-preview-wrap",
+						-- Scroll preview window
+						["<C-d>"] = "preview-page-down",
+						["<C-u>"] = "preview-page-up",
+						-- ["<S-left>"] = "preview-page-reset",
+					},
+				},
+			})
 
 			-- Find
 			vim.keymap.set("n", "<leader>if", fzflua.files, { desc = "[f]iles in cwd" })
