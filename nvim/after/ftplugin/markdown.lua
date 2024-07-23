494ad35bdd24917104e32d6673b2d36bd5d7e0aa
mak-leibniz <makmiller@gmail.com>
2024-07-23 00:12:37 -0400

    Remove unnecessary code
    Marksman provides this functionality

--- nvim/after/ftplugin/markdown.lua
+++ nvim/after/ftplugin/markdown.lua
@@ -3,21 +3,3 @@ vim.opt_local.spell = false
 
 -- Unfold first level headers at startup
 vim.opt_local.foldlevel = 1
-
--- Custom function for searching markdown headers
-function MarkdownHeader()
-	require("telescope.builtin").grep_string({
-		prompt_title = "Markdown Headers",
-		use_regex = true,
-		search = "^#",
-		search_dirs = { vim.fn.expand("%") },
-	})
-end
-
-vim.api.nvim_buf_set_keymap(
-	0,
-	"n",
-	"<localleader>mh",
-	"<cmd>lua MarkdownHeader()<CR>",
-	{ noremap = true, silent = true }
-)
