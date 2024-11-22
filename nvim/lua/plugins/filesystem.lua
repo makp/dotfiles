ffbd8e559b32c5222339360905bb94a7a3add267
mak-turing <makmiller@gmail.com>
2024-11-22 11:53:22 -0500

    Integrate yazi into nvim

--- nvim/lua/plugins/filesystem.lua
+++ nvim/lua/plugins/filesystem.lua
@@ -71,4 +71,29 @@ return {
 			vim.keymap.set("n", "<leader>o-", "<cmd>Oil<cr>", { desc = "parent directory" })
 		end,
 	},
+
+	{
+		"mikavilpas/yazi.nvim",
+		event = "VeryLazy",
+		keys = {
+			-- 👇 in this section, choose your own keymappings!
+			-- {
+			-- 	"<leader>of",
+			-- 	"<cmd>Yazi<cr>",
+			-- 	desc = "Open yazi at the current file",
+			-- },
+			{
+				"<leader>od",
+				"<cmd>Yazi cwd<cr>",
+				desc = "Open the file manager in nvim's working directory",
+			},
+			{
+				-- NOTE: this requires a version of yazi that includes
+				-- https://github.com/sxyazi/yazi/pull/1305 from 2024-07-18
+				"<leader>oD",
+				"<cmd>Yazi toggle<cr>",
+				desc = "Resume the last yazi session",
+			},
+		},
+	},
 }
