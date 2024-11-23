4ee3ab667df4cca43dbe471e9d957dbfd5c79f68
mak-turing <makmiller@gmail.com>
2024-11-23 09:41:45 -0500

    Use tmux gruvbox theme

--- scripts/term_scratchpad.sh
+++ scripts/term_scratchpad.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
-alacritty --title 'term_scratchpad' \
-	--option window.dimensions.columns=88 \
-	--option window.dimensions.lines=14 \
-	# --command zsh -c 'tmux attach -d'
+# alacritty --title 'term_scratchpad' \
+# 	--option window.dimensions.columns=88 \
+# 	--option window.dimensions.lines=14 \
+# --command zsh -c 'tmux attach -d'
+#
+#
+kitty --title="term_scratchpad" \
+  -o remember_window_size=n \
+  -o initial_window_height=14c \
+  -o initial_window_width=88c
