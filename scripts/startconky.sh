d558afe1a9ffa2f4ddb0c3cb000326c5c0f2d5d1
mak-leibniz <makmiller@gmail.com>
2024-11-24 22:08:12 -0500

    Del unused script

--- scripts/startconky.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-conky -c /home/makmiller/config-files/general/x-configs/conky-main &
-# conky -c ~/.conkyrcs/conky-middle --alignment=top_middle &
-# conky -c ~/.conkyrcs/conky-messages --alignment=top_right &
