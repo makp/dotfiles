33ec183a2a5e8f93755bd981dc98e974febd0dfc
mak-turing <makmiller@gmail.com>
2024-12-27 13:44:58 -0500

    Use tmux with alacritty; Update x-config submodule commit

--- scripts/create-term-scratchpad-window.sh
+++ scripts/create-term-scratchpad-window.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
-# alacritty --title 'term_scratchpad' \
-# 	--option window.dimensions.columns=88 \
-# 	--option window.dimensions.lines=14 \
+alacritty --title 'term_scratchpad' \
+  --option window.dimensions.columns=88 \
+  --option window.dimensions.lines=14
+#
 # --command zsh -c 'tmux attach -d'
 #
 #
-kitty --title="term_scratchpad" \
-  -o remember_window_size=n \
-  -o initial_window_height=14c \
-  -o initial_window_width=88c
+# kitty --title="term_scratchpad" \
+#   -o remember_window_size=n \
+#   -o initial_window_height=14c \
+#   -o initial_window_width=88c
