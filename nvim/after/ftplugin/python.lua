bc305db198e41d1d9c0893a92411b1a19c09dc20
mak-leibniz <makmiller@gmail.com>
2025-01-10 00:58:06 -0500

    Update sub commit; Add template for buffer-local toggles

--- nvim/after/ftplugin/python.lua
+++ nvim/after/ftplugin/python.lua
@@ -5,3 +5,9 @@ vim.api.nvim_buf_set_keymap(
 	"<cmd>80vsplit term://ipython<CR>",
 	{ desc = "open REPL for Python" }
 )
+
+--[[ -- C-x/C-a toggles for Python
+local existing_toggles = vim.b.CtrlXA_Toggles or vim.g.CtrlXA_Toggles or {}
+vim.b.CtrlXA_Toggles = vim.list_extend({
+	{ "print", "feio", "nada", "naosei" },
+}, existing_toggles) ]]
