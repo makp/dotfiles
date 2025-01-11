d275a48ea78b1525c2ca5a35d8bc99409563cbb7
mak-turing <makmiller@gmail.com>
2025-01-11 12:40:01 -0500

    Display path in oil's winbar

--- nvim/lua/plugins/filesystem.lua
+++ nvim/lua/plugins/filesystem.lua
@@ -42,6 +42,17 @@ return {
 		opts = {},
 		dependencies = { "nvim-tree/nvim-web-devicons" },
 		config = function()
+			-- Define global function to display current dir
+			function _G.get_oil_winbar()
+				local bufnr = vim.api.nvim_win_get_buf(vim.g.statusline_winid)
+				local dir = require("oil").get_current_dir(bufnr)
+				if dir then
+					return vim.fn.fnamemodify(dir, ":~")
+				else
+					-- If there is no current directory (e.g. over ssh), just show the buffer name
+					return vim.api.nvim_buf_get_name(0)
+				end
+			end
 			require("oil").setup({
 				skip_confirm_for_simple_edits = true,
 				view_options = {
@@ -66,6 +77,9 @@ return {
 					["g."] = "actions.cd", -- it was toggle_hidden
 					["<C-h>"] = "actions.toggle_hidden", -- it was hor split
 					["<C-s>"] = "actions.change_sort", -- it was vert split
+				},
+				win_options = {
+					winbar = "%!v:lua.get_oil_winbar()",
 				},
 			})
 			vim.keymap.set("n", "<leader>o-", "<cmd>Oil<cr>", { desc = "parent directory" })
