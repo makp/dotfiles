de06aab289e15b722cf791c436c8dc9fe08cc7ec
mak-turing <makmiller@gmail.com>
2025-01-14 12:58:29 -0500

    Enable GhostText plugin

--- nvim/lua/plugins/windows.lua
+++ nvim/lua/plugins/windows.lua
@@ -13,9 +13,24 @@ return {
 		},
 	},
 
-	-- Open nvim windows in browser
+	-- Edit text areas in web browsers with Neovim
+	-- {
+	-- 	"glacambre/firenvim",
+	-- 	build = ":call firenvim#install(0)",
+	-- },
 	{
-		"glacambre/firenvim",
-		build = ":call firenvim#install(0)",
+		"subnut/nvim-ghost.nvim",
+		keys = {
+			{ "<leader>lw", desc = "Enable sync with [w]eb browser." },
+		},
+		config = function()
+			vim.api.nvim_create_autocmd("User", {
+				group = "nvim_ghost_user_autocommands",
+				pattern = "*.com",
+				callback = function()
+					vim.bo.filetype = "markdown"
+				end,
+			})
+		end,
 	},
 }
