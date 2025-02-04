f37e6d85ef57dc3acac62b5d2fa9ca46ca2fa30d
mak-leibniz <makmiller@gmail.com>
2025-02-04 00:15:29 -0500

    feat(nvim): update lazydev installation instructions

--- nvim/lua/plugins/semantic.lua
+++ nvim/lua/plugins/semantic.lua
@@ -78,14 +78,9 @@ return {
 				library = {
 					-- See the configuration section for more details
 					-- Load luvit types when the `vim.uv` word is found
-					{ path = "luvit-meta/library", words = { "vim%.uv" } },
+					{ path = "${3rd}/luv/library", words = { "vim%.uv" } },
 				},
 			},
-		},
-		{
-			-- `vim.uv` typings
-			"Bilal2453/luvit-meta",
-			lazy = true,
 		},
 		{
 			-- Completion source for require statements and module annotations
