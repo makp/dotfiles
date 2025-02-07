3081096134b9a4470fd8ec340f2fe7a708d94a77
mak-leibniz <makmiller@gmail.com>
2025-02-07 00:11:44 -0500

    feat(aliases): add an alias to open a scratch buffer in neovim

--- zshrc.d/aliases.zsh
+++ zshrc.d/aliases.zsh
@@ -50,6 +50,7 @@ alias eh="nvim ."
 alias eu="run_fzf-on-files-and-dirs.sh"
 alias ea="run_rg.sh"
 alias er="nvim +'FzfLua oldfiles cwd_only=true'"
+alias es="nvim +'lua SwitchToScratch()'"
 
 # Git
 alias gt="lazygit"
